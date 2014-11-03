9411e0de441c71f9ab7d9cf46a733759ce20a084
Jean-Charles Leneveu <leneveu.jc@gmail.com>
2014-11-03 12:22:40 +0000

    Trying to debug angularjs problems

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 (function(angular){
 	var app = angular.module('app', []);
 	
-	.controller('form-controller', function($scope) {
+	app.controller('form-controller', function($scope) {
 		$scope.formData = [];
 		
-		$scope.myInput = 'sup?';
-		$scope.$watch('myInput', function(){
+		$scope.myInput = '';
+		$scope.$watch('myInput', function() {
 			$scope.formData.push($scope.myInput);
-		}
+		});
 		
 		$scope.submitform = function() {
 			$scope.formData.push($scope.myInput);
