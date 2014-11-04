27846fb783593b335126aa8eefc4feb2d6acc66d
Jean-Charles Leneveu <leneveu.jc@gmail.com>
2014-11-04 10:21:12 +0000

    Moved dumb data to js + refactored naming convention of input

--- app.js
+++ app.js
@@ -3,14 +3,38 @@
 	
 	app.controller('form-controller', function($scope) {
 		$scope.formData = [];
+
+		$scope.notifications = [{sender:'John', text:'text from john', context: "home"},
+			{sender:'Mary', text:'text from mary', context: "home"},
+			{sender:'Mike', text:'text from mike', context: "home"},
+			{sender:'Adam', text:'text from adam', context: "home"},
+			{sender:'Julie', text:'text from julie', context: "home"},
+			{sender:'Juliette', text:'text from juliette', context: "home"},
+			{sender:'Mary', text:'text from mary', context: "work"},
+			{sender:'Mike', text:'text from mike', context: "work"},
+			{sender:'Adam', text:'text from adam', context: "work"},
+			{sender:'Julie', text:'text from julie', context: "work"},
+			{sender:'Juliette', text:'text from juliette', context: "work"},
+			{sender:'Mary', text:'text from mary', context: "street"},
+			{sender:'Mike', text:'text from mike', context: "street"},
+			{sender:'Adam', text:'text from adam', context: "street"},
+			{sender:'Julie', text:'text from julie', context: "street"},
+			{sender:'Juliette', text:'text from juliette', context: "street"},
+			{sender:'Mary', text:'text from mary', context: "busy"},
+			{sender:'Mike', text:'text from mike', context: "busy"},
+			{sender:'Adam', text:'text from adam', context: "busy"},
+			{sender:'Julie', text:'text from julie', context: "busy"},
+			{sender:'Juliette', text:'text from juliette', context: "busy"}
+		];
 		
-		$scope.myInput = 'home';
-		$scope.$watch('myInput', function(){
-			$scope.formData.push($scope.myInput);
+		$scope.search = 'home';
+
+		$scope.$watch('search', function(){
+			$scope.formData.push($scope.search);
 		});
 		
 		$scope.submitform = function() {
-			$scope.formData.push($scope.myInput);
+			$scope.formData.push($scope.search);
 		}
 	});
 } (angular));
